e120128d73b01346bdbdcb45048f3a15e72330bc
Cesar Lujan <cesar.lujan@bankaya.com.mx>
2024-03-24 00:04:42 -0600

    feat: Implemented get phrase

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from typing import Union
+from random import randint
 
 from fastapi import FastAPI
 
 from models import Item
+from utils.utils import file_opener
 
 app = FastAPI()
 
@@ -15,6 +17,18 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/phrase")
+def get_phrase():
+    # TODO: Change for Database info
+    phrases = file_opener("Filosofía.md")
+    phrase_id = randint(0, len(phrases) - 1)
+    return {
+        "id": phrase_id,
+        "phrase": phrases[phrase_id][0],
+        "author": phrases[phrase_id][1]
+        }
 
 
 @app.put("/items/{item_id}")
